a5bd5143d78f880e433fbd71f0f7a00562e61b64
Raiz Sonora <mrbrey18@gmail.com>
2026-08-28 15:01:49 -0400

    Add centralized media configuration

---
+++ src/data/media.ts
@@ -0,0 +1,22 @@
+export const youtubeChannelUrl = "https://www.youtube.com/user/mrbreyrap";
+
+export const spotifyPlaylist =
+  "https://open.spotify.com/embed/playlist/37i9dQZF1DZ06evO3MhZgk?utm_source=generator";
+
+export const campaignYoutubeIds = [
+  "qOKoaWX6ArU",
+  "n7glKI_Di2E",
+  "rIXRTSDiWkg",
+] as const;
+
+export const archiveFallbackVideos = [
+  { title: "Deambulando", youtubeId: "_Lr1Vwnv5fM" },
+  { title: "Creo mundo con mis versos", youtubeId: "DK9j0gRpSoY" },
+  { title: "No hay Ayer ni mañana", youtubeId: "nTWWyZJZgZY" },
+  { title: "Al verla a Ella", youtubeId: "w7w1iQghmCo" },
+  { title: "Pensando en ti", youtubeId: "oe3FcO90Siw" },
+  { title: "Sobrenatural", youtubeId: "BYOLv1vQP0c" },
+  { title: "No Me Digas que me quieres", youtubeId: "MatMK_l2lVw" },
+  { title: "Quiere ir de fiesta", youtubeId: "2watUetASk4" },
+  { title: "Alquimista", youtubeId: "6TYVRVhOexk" },
+] as const;
